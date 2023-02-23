197c9724d1a2202d114316c2bf0c7192e8ec08dd
MennaSalah03 <menna.skh2003@gmail.com>
2023-02-23 16:43:07 +0200

    negavive positive

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - debugging the finction positive_or_negative
+ * Return: 0 (success)
+ */
+
+int mainc(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
