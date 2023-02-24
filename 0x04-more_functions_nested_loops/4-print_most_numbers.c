08e3cab2c146d43b11ebad6bb790a7c82d6eadc7
MennaSalah03 <menna.skh2003@gmail.com>
2023-02-25 00:26:54 +0200

    modify

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,12 +2,11 @@
 
 /**
  * print_most_numbers - Print numbers between 0 to 9 excluding 2 and 4
- *
- * Return: Void.
  */
 void print_most_numbers(void)
 {
 	int num;
+
 	for (num = 48; num >= 57; num++)
 	{
 		if (num == 2 || num == 4)
