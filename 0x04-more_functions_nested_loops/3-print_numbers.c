4ff427a0b07be5a2a98f022708e16aac777da60a
MennaSalah03 <menna.skh2003@gmail.com>
2023-02-25 00:24:15 +0200

    modify

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - Print numbers between 0 to 9 
+ * print_numbers - Print numbers between 0 to 9,
+ * followed by a new line
  */
 
 void print_numbers(void)
