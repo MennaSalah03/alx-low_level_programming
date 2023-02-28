360c55067ca9aaae860f727aa6105462363c7da8
MennaSalah03 <menna.skh2003@gmail.com>
2023-02-28 03:12:57 +0200

    second

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: the string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int i, j, size;
+	char tmp;
+	
+	i = 0;
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	size = i - 1;
+	j = 0;
+	for (j = 0; j < size / 2; j++)
+	{
+		tmp = s[j];
+		s[j] = s[size];
+		s[size] = tmp;
+		size--;
+	}
+}
