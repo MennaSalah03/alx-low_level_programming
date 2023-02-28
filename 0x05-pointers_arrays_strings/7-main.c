33523706644dc559d717b3eb37c8993413b908a8
MennaSalah03 <menna.skh2003@gmail.com>
2023-02-28 15:10:11 +0200

    half string

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "Lrmismdlrstae,cnettraiicn lt e oeumdtmo niiutu aoee ooemgaaiu.U nma ii eim usnsrdeectto lac aoi iiu lqi xe omd osqa.Di ueiuedlri erhnei nvlpaevltes ilmdlr ufga ul aitr xetu itocea uiaa o riet uti up u fii eeutmli nmi s aou.";
+    puts_half(str);
+    return (0);
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to be half-printed
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	for (i = i / 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+}
