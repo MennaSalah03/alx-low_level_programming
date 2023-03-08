5de796fa3c29c7c8ab52f0957d4f4149484dbcf2
MennaSalah03 <menna.skh2003@gmail.com>
2023-03-08 22:24:54 +0200

    update 1

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,6 +5,7 @@
  * main - contain prototypes to functions used int this directory
  */
 
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
