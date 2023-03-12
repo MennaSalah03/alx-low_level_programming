9f6ef0f7274570bb43e8f5e181a9683bef982c14
MennaSalah03 <menna.skh2003@gmail.com>
2023-03-13 01:22:29 +0200

    multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,10 +17,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		product = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d", product);
-		return (product);
-	}
+	product = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", product);
+	return (product);
 }	
