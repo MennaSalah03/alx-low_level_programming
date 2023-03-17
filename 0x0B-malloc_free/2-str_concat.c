f61a6f89343638ebed4064e4972cb3cb9f23f017
MennaSalah03 <menna.skh2003@gmail.com>
2023-03-17 12:44:17 +0200

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,9 +20,8 @@ char *str_concat(char *s1, char *s2)
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 	j++;
-
 	dest = malloc((i + j) * sizeof(char));
-	for (i =0; s1[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
 		dest[i] = s1[i];
 	}
@@ -31,5 +30,10 @@ char *str_concat(char *s1, char *s2)
 		dest[i] = s2[j];
 		i++;
 	}
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	dest[i] = '\0';
 	return (dest);
 }
