76b412f1613432229f0c26b2fc9d22de68d8b0ec
MennaSalah03 <menna.skh2003@gmail.com>
2023-03-24 00:44:53 +0200

    fixing error in print nums

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,31 +3,32 @@
 #include <stdio.h>
 /**
  * print_numbers - as name suggests, it prints numbers given as args
- * @separator: string 
- * @n: counts the args
+ * @separator: string that separates printed numbers
+ * @n: count of the args
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	va_list ptr;
+	va_list ap;
 
 
-	va_start(ptr, n);
+	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
 		if (separator == NULL)
 		{
 			break;
 		}
-		if (i != n)
+		if (i < n)
 		{
-		printf("%d%s", va_arg(ptr, int), separator);
+			printf("%d", va_arg(ap, int));
 		}
-		else
+		if (i != n - 1)
 		{
-			printf("%d", va_arg(ptr, int));
+			printf("%s", separator);
 		}
 	}
-	va_end(ptr);
+	printf("\n");
+	va_end(ap);
 }
