cbf19d6abccd51cf82bf6eaf1cfd6ba5cb3c24d7
MennaSalah03 <102877178+MennaSalah03@users.noreply.github.com>
2023-06-24 22:53:11 +0300

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/0-object_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef OBJECTLIKEMACRO_H
-#define OBJECTLIKEMACRO_H
-
-#define SIZE 1024
-
-#endif
--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,7 +0,0 @@
-#ifndef FUNCTIONLIKEMACRO_H
-#define FUNCTIONLIKEMACRO_H
-
-#define ABS(x) ((x) < (0) ? -(x) : (x))
-
-
-#endif
