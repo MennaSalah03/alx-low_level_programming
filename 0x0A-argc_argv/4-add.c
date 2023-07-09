b455ad0a11797271df2d7b0ef24f19080d4854dd
MennaSalah03 <menna.skh2003@gmail.com>
2023-07-09 19:24:21 +0300

    Task 4 attempt 3

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,19 +4,19 @@
 
 /**
  * main - adds positive numbers
- *
  * @argc: count of arguments
- *
  * @argv: pointer to argumments as strings
+ *
+ * Return: The sum of entered numbers
  */
 
 int main(int argc, char *argv[])
 {
 	int i,  res = 0;
+
 	for (i = 1; i < argc; i++)
 	{
-		
-		if (!atoi(argv[i]))
+		if (! atoi(argv[i]) || ! (atoi(argv[i]) > 0))
 		{
 			printf("Error\n");
 			return (1);
