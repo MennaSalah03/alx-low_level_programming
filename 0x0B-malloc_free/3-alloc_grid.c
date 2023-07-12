ca369a417bf35f1455968c5d5fb68c27289a0ca4
MennaSalah03 <menna.skh2003@gmail.com>
2023-07-13 01:06:11 +0300

    Task 3 attempt 4

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,17 +21,25 @@ int **alloc_grid(int width, int height)
 	}
 
 	arr = (int **) malloc(sizeof(int *) * height);
-	for (i = 0; i < height; i++)
-	{
-		arr[i] = (int *) malloc(sizeof(int) * width);
-	}
-
 	if (arr == NULL)
 	{
 		free(arr);
 		return (NULL);
 	}
 
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = (int *) malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			for (; i >= 0; i--)
+			{
+				free(arr[i]);
+			}
+			free (arr);
+			return (NULL);
+		}
+	}
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
