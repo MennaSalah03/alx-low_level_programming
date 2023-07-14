4957670e6bf145f7bf536be35c718197b760257b
MennaSalah03 <menna.skh2003@gmail.com>
2023-07-15 01:09:51 +0300

    Task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - allocates memory and make all values 0
+ * @nmemb: number of blocks to be allocated
+ * @size: The size of each block
+ *
+ * Return: NULL pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *ptr;
+	
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+		ptr [i] = 0;
+	}
+	return (ptr);
+}
