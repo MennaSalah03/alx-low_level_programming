1fb7b40307c03b449c463ac9942d5aa88269fc1a
MennaSalah03 <menna.skh2003@gmail.com>
2023-07-17 18:51:05 +0300

    Task 2 attempt 1

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the name of the current file
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
