23b5a19de7d5a934fdaa8ca4f6ccdba42b2a7a9c
MennaSalah03 <menna.skh2003@gmail.com>
2023-07-20 23:10:35 +0300

    Task 2 Attempt 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - prints strings with separators
+ * @separator: the separator string
+ * @n: number of strings
+ * Return: nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+	char *ptr;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		ptr = va_arg(ap, char *);
+		if (ptr != NULL)
+		{
+		printf("%s", ptr);
+		}
+		else
+		{
+			puts("(nil)");
+		}
+		if (separator != NULL && i != n - 1)
+		{
+			printf("%s", separator);
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+	va_end(ap);
+}
