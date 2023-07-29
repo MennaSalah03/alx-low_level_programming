cc73ea7064a85f82da7e63a0bba91466d040ddac
MennaSalah03 <menna.skh2003@gmail.com>
2023-07-30 02:21:21 +0300

    3-2

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,7 +10,7 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	int i;
+	int i = 0;
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -19,17 +19,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	for (i = 0; ops[i].op; i++)
+	while (ops[i].op)
 	{
-		if (ops[i].op[1] == '\0')
-		{
-			printf("Error\n");
-			exit(99);
-		}
 		if (ops[i].op[0] == s[0])
 		{
 			return (ops[i].f);
 		}
+		i++;
 	}
 	return (0);
 }
