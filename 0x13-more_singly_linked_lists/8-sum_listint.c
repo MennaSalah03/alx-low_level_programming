fa5af1555592a546378a43ac656c08712af20340
MennaSalah03 <menna.skh2003@gmail.com>
2023-07-31 23:15:15 +0300

    Task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_listint - gives the sum of all the numbers in the list
+ * @head: the address to first node in the list
+ *
+ * Return: the sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *ptr;
+	
+	if (head == NULL)
+		return (0);
+	ptr = head;
+	while (ptr != NULL)
+	{
+		sum += ptr->n;
+		ptr = ptr->next;
+	}
+	return (sum);
+}
