1853b817f48ab32604b7153218e9bc24475607ab
MennaSalah03 <menna.skh2003@gmail.com>
2023-07-31 21:08:31 +0300

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listinr2 - frees the structure where the list is formed upon
+ * @head: pointer to address of the first element
+ *
+ * return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr1 = *head, *ptr2;
+
+	while (ptr1 != NULL)
+	{
+		ptr2 = ptr1->next;
+		free(ptr1);
+		ptr1 = ptr2;
+	}
+	*head = NULL;
+}
